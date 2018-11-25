8a75c568b844c488642f234fcd92eba522ab1a70
Kurt <kaphotics@gmail.com>
2018-11-24 21:55:48 -0800

    Add centralized source for randomizer settings objs

---
+++ pkNX.WinForms/MainEditor/EditUtil.cs
@@ -0,0 +1,13 @@
+﻿using pkNX.Randomization;
+
+namespace pkNX.WinForms
+{
+    public static class EditUtil
+    {
+        public static PersonalRandSettings Personal { get; set; } = new PersonalRandSettings();
+        public static SpeciesSettings Species { get; set; } = new SpeciesSettings();
+        public static TrainerRandSettings Trainer { get; set; } = new TrainerRandSettings();
+        public static MovesetRandSettings Move { get; set; } = new MovesetRandSettings();
+        public static LearnSettings Learn { get; set; } = new LearnSettings();
+    }
+}
