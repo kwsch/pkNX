1e43af3e2be325b91a10d802a42cfe10348cbf09
sora10pls <sorafanatic10@gmail.com>
2020-03-30 17:03:36 -0400

    Document EncounterTrade8 If anybody is able to figure out UnknownRequirement, please PR! Now with most Gift/Trade fields documented, it should make DLC discoveries much easier. Also update summary output to match PKHeX.

--- pkNX.Structures/Encounter/Gen8/FlatTrade/EncounterTrade8Archive.cs
+++ pkNX.Structures/Encounter/Gen8/FlatTrade/EncounterTrade8Archive.cs
@@ -9,57 +9,59 @@ namespace pkNX.Structures
 
     public class EncounterTrade8
     {
-        public byte Field_00 { get; set; }
-        public byte DynamaxLevel { get; set; }
-        public uint Field_02 { get; set; }
-        public byte Field_03 { get; set; }
+        public int AltForm { get; set; }
+        public int DynamaxLevel { get; set; }
+        public int Ball { get; set; }
+        public int Field_03 { get; set; }
         public ulong Hash0 { get; set; }
-        public byte AltForm { get; set; } // ???
-        public uint HeldItem { get; set; }
-        public byte Level { get; set; }
-        public uint Species { get; set; }
+        public bool CanGigantamax { get; set; }
+        public int HeldItem { get; set; }
+        public int Level { get; set; }
+        public int Species { get; set; }
         public ulong Hash1 { get; set; }
-        public uint TrainerID { get; set; }
-        public byte Field_0B { get; set; }
-        public byte Field_0C { get; set; }
-        public byte Field_0D { get; set; }
-        public byte Field_0E { get; set; }
+        public int TrainerID { get; set; }
+        public int Memory { get; set; }
+        public int TextVar { get; set; }
+        public int Feeling { get; set; }
+        public int Intensity { get; set; }
         public ulong Hash2 { get; set; }
-        public byte OTGender { get; set; }
-        public uint RequiredForm { get; set; }
-        public uint RequiredSpecies { get; set; }
-        public uint Nature { get; set; }
-        public byte Memory { get; set; }
-        public uint Argument { get; set; }
-        public uint Feeling { get; set; }
-        public uint Intensity { get; set; }
-        public byte IV_Hp { get; set; }
-        public byte IV_Atk { get; set; }
-        public byte IV_Def { get; set; }
-        public byte IV_SpAtk { get; set; }
-        public byte IV_SpDef { get; set; }
-        public byte IV_Spe { get; set; }
-        public byte Ability { get; set; }
-        public ushort Relearn1 { get; set; }
-        public ushort Relearn2 { get; set; }
-        public ushort Relearn3 { get; set; }
-        public ushort Relearn4 { get; set; }
+        public int OTGender { get; set; }
+        public int RequiredForm { get; set; }
+        public int RequiredSpecies { get; set; }
+        public int RequiredNature { get; set; }
+        public int UnknownRequirement { get; set; } // all 0; we know this field is a trade requirement, but unsure what exactly
+        public int ShinyLock { get; set; }
+        public int Nature { get; set; }
+        public int Gender { get; set; }
+        public int IV_Hp { get; set; }
+        public int IV_Atk { get; set; }
+        public int IV_Def { get; set; }
+        public int IV_SpAtk { get; set; }
+        public int IV_SpDef { get; set; }
+        public int IV_Spe { get; set; }
+        public int Ability { get; set; }
+        public int Relearn1 { get; set; }
+        public int Relearn2 { get; set; }
+        public int Relearn3 { get; set; }
+        public int Relearn4 { get; set; }
 
-        private ushort[] Relearn => new[] {Relearn1, Relearn2, Relearn3, Relearn4};
+        private int[] Relearn => new[] {Relearn1, Relearn2, Relearn3, Relearn4};
 
         public string GetSummary(IReadOnlyList<string> species)
         {
-            var comment = $" // {species[(int)Species]}{(AltForm == 0 ? string.Empty : "-" + AltForm)}";
+            var comment = $" // {species[Species]}{(AltForm == 0 ? string.Empty : "-" + AltForm)}";
             var ability = $"Ability = {Ability}";
-            var gender = $", OTGender = {OTGender}";
+            var otgender = $", OTGender = {OTGender}";
+            var gender = $", Gender = {Gender}";
             var altform = AltForm == 0 ? string.Empty : $", Form = {AltForm}";
+            var giga = !CanGigantamax ? string.Empty : ", CanGigantamax = true";
             var tid = $", TID7 = {TrainerID}";
             var dyna = $", DynamaxLevel = {DynamaxLevel}";
             var relearn = Relearn1 == 0 ? "                                   " : $", Relearn = new[] {{{Relearn1},{Relearn2},{Relearn3},{Relearn4}}}";
             const string iv = ", IVs = TradeIVs";
 
             return
-                $"            new EncounterTrade8({Species:000},{Level:00}) {{ {ability}{tid}{iv}{dyna}{gender}{altform}{relearn} }},{comment}";
+                $"            new EncounterTrade8({Species:000},{Level:00},{Memory:00},{TextVar:000},{Feeling:00},{Intensity}) {{ {ability}{tid}{iv}{dyna}{otgender}{gender}{altform}{giga}{relearn} }},{comment}";
         }
     }
 }
