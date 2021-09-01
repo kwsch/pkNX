0ddea6cf13b4e79c25a0c37291fb6c60b3826b29
Kurt <kaphotics@gmail.com>
2021-09-01 10:38:08 -0700

    Startup hack
    Instead of using the compile-time serializer building, we'll opt to use the run-time generating so that we can have more complexity (interfaces, etc) in our object classes.

--- pkNX.WinForms/Program.cs
+++ pkNX.WinForms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace pkNX.WinForms
@@ -11,6 +12,10 @@ namespace pkNX.WinForms
         [STAThread]
         private static void Main()
         {
+            // Make FlatSharp build the serializers for our FlatBuffer objects now and take the startup penalty async
+            // Opening a FlatBuffer editor later won't be hit with a >5s delay assuming the user opens the editor no earlier than 10 seconds after program startup.
+            _ = Task.Run(() => _ = Structures.FlatBuffers.FlatBufferConverter.SerializeFrom(new Structures.FlatBuffers.Waza8()));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
