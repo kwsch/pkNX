d6c8780ed266dc8ba6bc977db7269084af170b2e
Kurt <kaphotics@gmail.com>
2021-10-23 14:10:04 -0700

    FishSpot IterateForSlotsExceptLastN

--- pkNX.Structures.FlatBuffers/Gen8/Placement/Zone/Holders/PlacementZone8FishingPointHolder.cs
+++ pkNX.Structures.FlatBuffers/Gen8/Placement/Zone/Holders/PlacementZone8FishingPointHolder.cs
@@ -11,13 +11,15 @@ namespace pkNX.Structures.FlatBuffers
     [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
     public class PlacementZone8FishingPointHolder
     {
-        [FlatBufferItem(00)] public PlacementZone8FishingPoint Field_00 { get; set; } = new();
+        [FlatBufferItem(00)] public PlacementZone8FishingPoint Object { get; set; } = new();
+
+        public override string ToString() => $"{Object.Identifier}" + (Object.IterateForSlotsExceptLastN == 0 ? "" : $" SkipLast{Object.IterateForSlotsExceptLastN}");
     }
 
     [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
     public class PlacementZone8FishingPoint
     {
-        [FlatBufferItem(00)] public PlacementZoneMetaTripleXYZ8 Field_00 { get; set; } = new();
+        [FlatBufferItem(00)] public PlacementZoneMetaTripleXYZ8 Identifier { get; set; } = new();
         [FlatBufferItem(01)] public float Field_01 { get; set; }
         [FlatBufferItem(02)] public float Field_02 { get; set; }
         [FlatBufferItem(03)] public float Field_03 { get; set; }
@@ -25,6 +27,8 @@ namespace pkNX.Structures.FlatBuffers
         [FlatBufferItem(05)] public float Field_05 { get; set; }
         [FlatBufferItem(06)] public float Field_06 { get; set; }
         [FlatBufferItem(07)] public float Field_07 { get; set; }
-        [FlatBufferItem(08)] public uint Field_08 { get; set; }
+
+        [FlatBufferItem(08), Description("When iterating over slots to pick a random one, the iteration will skip the last (value) amount of slots.")]
+        public uint IterateForSlotsExceptLastN { get; set; }
     }
 }
