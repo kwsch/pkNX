b15e5b8cef03437fb187c43f77a39a0c8738af57
Jelle <60387522+JelleInfinity@users.noreply.github.com>
2022-10-02 20:45:19 +0200

    Add support for categorized editors

--- pkNX.WinForms/Main.cs
+++ pkNX.WinForms/Main.cs
@@ -177,13 +177,64 @@ public partial class Main : Form
     private const int ButtonHeight = 40;
     private const int ButtonPadding = 3;
 
+    private void LoadEditorButtons(EditorCategory category = EditorCategory.None)
+    {
+        FLP_Controls.SuspendLayout();
+        FLP_Controls.Controls.Clear();
+
+        if (category == EditorCategory.None)
+        {
+            foreach (var c in (EditorCategory[])Enum.GetValues(typeof(EditorCategory)))
+            {
+                if (c == EditorCategory.None)
+                    continue;
+
+                if (Editor!.CountControlsForCategory(c) == 0)
+                    continue;
+
+                FLP_Controls.Controls.Add(CreateCategoryButton(c));
+            }
+        }
+        else
+        {
+            // Create back button
+            FLP_Controls.Controls.Add(CreateCategoryButton(EditorCategory.None));
+        }
+
+        AddEditorButtonsForCategory(category);
+
+        FLP_Controls.ResumeLayout();
+    }
+
+    private void AddEditorButtonsForCategory(EditorCategory category)
+    {
+        var ctrls = Editor!.GetControls(ButtonWidth, ButtonHeight, category).OrderBy(x => x.Text);
+
+        foreach (var ctrl in ctrls)
+            FLP_Controls.Controls.Add(ctrl);
+    }
+
+    public Button CreateCategoryButton(EditorCategory category)
+    {
+        var b = new Button
+        {
+            Width = ButtonWidth,
+            Height = ButtonHeight,
+            Name = $"B_OpenCategory{category}",
+            Text = ((category == EditorCategory.None) ? "Back" : $"Show {category} Editors"),
+        };
+        b.Click += (s, e) =>
+        {
+            LoadEditorButtons(category);
+        };
+        return b;
+    }
+
     private void LoadROM(EditorBase editor)
     {
         Editor = editor;
-        var ctrl = Editor.GetControls(ButtonWidth, ButtonHeight).OrderBy(x => x.Text);
-        FLP_Controls.Controls.Clear();
-        foreach (var c in ctrl)
-            FLP_Controls.Controls.Add(c);
+
+        LoadEditorButtons();
 
         const int wp = ButtonWidth + (2 * ButtonPadding) + 3;
         const int hp = ButtonHeight + (2 * ButtonPadding);
