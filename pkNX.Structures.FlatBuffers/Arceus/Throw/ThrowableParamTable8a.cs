811929932bd91df55876f87af6c2826993c43a6c
Jelle <60387522+JelleInfinity@users.noreply.github.com>
2022-10-16 12:38:31 +0200

    Added missing field

--- pkNX.Structures.FlatBuffers/Arceus/Throw/ThrowableParamTable8a.cs
+++ pkNX.Structures.FlatBuffers/Arceus/Throw/ThrowableParamTable8a.cs
@@ -22,14 +22,15 @@ public class ThrowableParamTable8a : IFlatBufferArchive<ThrowableParam8a>
 [FlatBufferTable, TypeConverter(typeof(ExpandableObjectConverter))]
 public class ThrowableParam8a
 {
-    [FlatBufferItem(00)] public int    ItemID { get; set; }
-    [FlatBufferItem(01)] public ulong  Hash_01 { get; set; }
-    [FlatBufferItem(02)] public ulong  ThrowPermissionSet  { get; set; }
-    [FlatBufferItem(03)] public ulong  ThrowParam          { get; set; }
-    [FlatBufferItem(04)] public ulong  Hash_04 { get; set; }
-    [FlatBufferItem(05)] public int    Field_05 { get; set; }
-    [FlatBufferItem(06)] public float  Field_06 { get; set; }
+    [FlatBufferItem(00)] public int ItemID { get; set; }
+    [FlatBufferItem(01)] public ulong Hash_01 { get; set; }
+    [FlatBufferItem(02)] public ulong ThrowPermissionSet { get; set; }
+    [FlatBufferItem(03)] public ulong ThrowParam { get; set; }
+    [FlatBufferItem(04)] public ulong Hash_04 { get; set; }
+    [FlatBufferItem(05)] public int Field_05 { get; set; }
+    [FlatBufferItem(06)] public float Field_06 { get; set; }
     [FlatBufferItem(07)] public string Label_01 { get; set; } = string.Empty;
     [FlatBufferItem(08)] public string Label_02 { get; set; } = string.Empty;
-    [FlatBufferItem(09)] public float  Field_09 { get; set; }
+    [FlatBufferItem(09)] public float Field_09 { get; set; }
+    [FlatBufferItem(10)] public int Field_10 { get; set; }
 }
