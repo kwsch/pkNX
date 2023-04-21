128d902d206ca6df258023ef34f57890c25ac236
sora10pls <17801814+sora10pls@users.noreply.github.com>
2023-04-21 08:52:25 -0400

    Update GameLocation.cs

--- pkNX.Game/GameLocation.cs
+++ pkNX.Game/GameLocation.cs
@@ -90,6 +90,7 @@ public sealed class GameLocation
     private const int FILECOUNT_SV = 24;
     private const int FILECOUNT_SV_101 = 25; // Ver. 1.0.1 (Day 1 Patch)
     private const int FILECOUNT_SV_120 = 26; // Ver. 1.2.0 (Paradox x2)
+    private const int FILECOUNT_SV_130 = 27; // Ver. 1.3.0 (Paradox x2 Bad Egg fix)
 
     private static GameVersion GetGameFromCount(int fileCount, string romfs, string? exefs)
     {
@@ -147,6 +148,7 @@ public sealed class GameLocation
             case FILECOUNT_SV:
             case FILECOUNT_SV_101:
             case FILECOUNT_SV_120:
+            case FILECOUNT_SV_130:
             {
                 if (exefs == null)
                     return GameVersion.SV;
