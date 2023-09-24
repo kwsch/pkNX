362c5791c609044befb629139c0ded3826df982e
sora10pls <17801814+sora10pls@users.noreply.github.com>
2023-09-24 18:57:33 -0400

    Add delivery outbreak fbs schemas

---
+++ pkNX.Structures.FlatBuffers.SV/pkNX.Structures.FlatBuffers.SV.World/World/DeliveryOutbreakPokeData.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel;
+// ReSharper disable UnusedMember.Global
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedType.Global
+#nullable disable
+
+namespace pkNX.Structures.FlatBuffers.SV;
+
+[TypeConverter(typeof(ExpandableObjectConverter))]
+public partial class DeliveryOutbreakPokeDataArray { }
+
+[TypeConverter(typeof(ExpandableObjectConverter))]
+public partial class DeliveryOutbreakPokeData { }
+
+[TypeConverter(typeof(ExpandableObjectConverter))]
+public partial class AreaNo { }
