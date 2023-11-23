5ad6d42f70ca215cd12cd5549417e92afd336502
Dr. Victor Vasconcelos de Souza <32229843+drvictorvs@users.noreply.github.com>
2023-11-23 14:13:53 -0300

    Update MoveInflict.cs

--- pkNX.Structures/Move/MoveInflict.cs
+++ pkNX.Structures/Move/MoveInflict.cs
@@ -5,29 +5,26 @@ namespace pkNX.Structures;
 [Flags]
 public enum MoveInflict : int
 {
-    None = 0,
-
-    Paralyze     = 1, 
-    Sleep           = 2, 
-    Freeze         = 3, 
-    Burn          = 4,
-    Poison      = 5,
-    Confusion           = 6,
-    Infatuation           = 7,
-    TransientDamage            = 8, // Used in Fire Spin, Wrap, etc.
-
-    Nightmare            = 9,
-    Torment             = 12,
-    Disable = 13,
-    Drowsiness    = 14,
-    HealBlock      = 15,
-
-    Identify            = 17, // Used by Odor Sleuth and whatnot
-    Seed              = 18, // Used by Leech Seed
-    Embargo              = 19,
-    SingPerish              = 20, // Used by Perish Song
-    Ingrain              = 21,
-    ThroatAche           = 24, // Used by Throat Chop
-    FireWeakness              = 42, // Used by Tar Shot
+  None = 0,
+  Paralyze = 1, 
+  Sleep = 2, 
+  Freeze = 3, 
+  Burn = 4,
+  Poison = 5,
+  Confusion = 6,
+  Infatuation = 7,
+  DamageTransiently = 8, // Used in Fire Spin, Wrap, etc.
+  Nightmare = 9,
+  Torment = 12,
+  Disable = 13,
+  Drowsiness = 14,
+  HealBlock = 15,
+  Identify = 17, // Used by Odor Sleuth and whatnot
+  Seed = 18, // Used by Leech Seed
+  Embargo = 19,
+  SingPerish = 20, // Used by Perish Song
+  Ingrain = 21,
+  ChopThroat = 24, // Used by Throat Chop
+  FireWeaken = 42, // Used by Tar Shot
 }
 
