72d99b41b8ceba9b02b55c9cd59e4028a7c85895
Kurt <kaphotics@gmail.com>
2025-02-02 03:55:58 -0600

    Minor clean

--- FlatBuffers/pkNX.Structures.FlatBuffers/FlatBufferConverter.cs
+++ FlatBuffers/pkNX.Structures.FlatBuffers/FlatBufferConverter.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Buffers;
-using System.IO;
 using FlatSharp;
 using static FlatSharp.FlatBufferDeserializationOption;
 
--- FlatBuffers/pkNX.Structures.FlatBuffers/Util/FlatDumper.cs
+++ FlatBuffers/pkNX.Structures.FlatBuffers/Util/FlatDumper.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
 using FlatSharp;
 
 namespace pkNX.Structures.FlatBuffers;
